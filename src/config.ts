2ce530076827016a97c146a663d08f02936dc6f3
wangjunwei <863445607@qq.com>
2020-07-05 00:33:24 +0800

    添加注释

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wangjunwei
+ * @Date: 2020-07-04 21:04:09
+ * @LastEditors: wangjunwei
+ * @LastEditTime: 2020-07-05 00:32:45
+ */ 
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wangjunwei
+ * @Date: 2020-07-04 21:05:33
+ * @LastEditors: wangjunwei
+ * @LastEditTime: 2020-07-04 21:56:57
+ */ 
+
