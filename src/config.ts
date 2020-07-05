6c0f3203fe0c804276d9a773871c5f3722a136e6
wangjunwei <863445607@qq.com>
2020-07-06 00:30:07 +0800

    update index.js

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,9 @@
  * @Author: wangjunwei
  * @Date: 2020-07-04 21:04:09
  * @LastEditors: wangjunwei
- * @LastEditTime: 2020-07-05 00:32:45
+ * @LastEditTime: 2020-07-05 23:17:29
  */ 
+
+ export const config = {
+
+ }
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,18 @@
  * @Author: wangjunwei
  * @Date: 2020-07-04 21:05:33
  * @LastEditors: wangjunwei
- * @LastEditTime: 2020-07-04 21:56:57
+ * @LastEditTime: 2020-07-05 23:09:54
  */ 
 
+//import { Wechaty } from 'wechaty'
+const { Wechaty } = require('wechaty')
+//import { PuppetPadplus } from 'wechaty-puppet-padplus'
+const { PuppetPadplus } = require('wechaty-puppet-padplus')
+let token = "puppet_padplus_0d3505e8f7001abe"
+
+
+Wechaty.instance()
+.on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.github.io/qrcode/${encodeURIComponent(qrcode)}`))
+.on('login',            user => console.log(`User ${user} logined`))
+.on('message',       message => console.log(`Message: ${message}`))
+.start()
