e075f6732c1c2212739304c316aeb061e0729bc4
wangjunwei <863445607@qq.com>
2020-07-06 16:16:57 +0800

    padplus login successful!

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,26 @@
  * @Author: wangjunwei
  * @Date: 2020-07-04 21:05:33
  * @LastEditors: wangjunwei
- * @LastEditTime: 2020-07-05 23:09:54
+ * @LastEditTime: 2020-07-06 16:13:43
  */ 
 
 //import { Wechaty } from 'wechaty'
 const { Wechaty } = require('wechaty')
 //import { PuppetPadplus } from 'wechaty-puppet-padplus'
 const { PuppetPadplus } = require('wechaty-puppet-padplus')
+//const config = require('./config.ts')
 let token = "puppet_padplus_0d3505e8f7001abe"
 
+const bot = new Wechaty({
+    puppet: new PuppetPadplus({
+      token: token
+    }),
+    name: "俊伟"
+  })
+  
 
-Wechaty.instance()
+//Wechaty.instance()
+bot
 .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.github.io/qrcode/${encodeURIComponent(qrcode)}`))
 .on('login',            user => console.log(`User ${user} logined`))
 .on('message',       message => console.log(`Message: ${message}`))
