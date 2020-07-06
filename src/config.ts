41bac78994ec3500575ef5b067f1df608e432432
王俊伟 <wangjunwei@wangjunweideMBP.lan>
2020-07-06 23:04:49 +0800

    合并

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@
  * @Author: wangjunwei
  * @Date: 2020-07-04 21:04:09
  * @LastEditors: wangjunwei
- * @LastEditTime: 2020-07-05 23:17:29
+ * @LastEditTime: 2020-07-06 10:57:52
  */ 
 
- export const config = {
-
+export const config = {
+     
  }
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@
  * @Author: wangjunwei
  * @Date: 2020-07-04 21:05:33
  * @LastEditors: wangjunwei
- * @LastEditTime: 2020-07-05 23:09:54
+ * @LastEditTime: 2020-07-06 11:02:49
  */ 
 
 //import { Wechaty } from 'wechaty'
 const { Wechaty } = require('wechaty')
 //import { PuppetPadplus } from 'wechaty-puppet-padplus'
 const { PuppetPadplus } = require('wechaty-puppet-padplus')
+const config = require('./config')
 let token = "puppet_padplus_0d3505e8f7001abe"
 
 
